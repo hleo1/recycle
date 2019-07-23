69e62cffcc80cbbad9365172544d248221bf453e
Tori Dakich <toridakich@Toris-MacBook-Air-2.local>
2019-07-23 14:55:27 +0200

    backend with frontend

---
+++ fs-consumer-app/src/app/services/user.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { calcBindingFlags } from '@angular/core/src/view/util';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+ 
+  users: Array<any>;
+  loggedInUser: any;
+
+  constructor(private httpClient: HttpClient) {
+    
+
+  }
+
+  public logIn(Authuser: any) { 
+
+    return new Promise((resolve, reject) => {
+      const headers = new HttpHeaders();
+      
+      this.httpClient.post(environment.BaseUrl + '/api/auth/login', Authuser, { headers }).subscribe((response: any)=>{
+        console.log(response.id);
+        localStorage.setItem('userId', response.id);
+        resolve(response);
+        
+      },
+      (err: any) => {
+        console.log(err);
+        reject(err);
+      }
+
+      );
+
+      
+    });
+
+  }
+
+  public addUser(newUser: any){
+    return new Promise((resolve, reject) => {
+      const headers = new HttpHeaders();
+      
+      this.httpClient.post(environment.BaseUrl + '/api/auth/register', newUser, { headers }).subscribe((response: any)=>{
+        console.log(response.id);
+        localStorage.setItem('userId', response.id);
+        resolve(response);
+        
+      },
+      (err: any) => {
+        console.log(err);
+        reject(err);
+      }
+
+      );
+    });
+  }
+
+  setLoggedInUser(user: any) { 
+    this.loggedInUser = user;
+  }
+
+  getLoggedInUser(): any { 
+    return this.loggedInUser;
+  }
+
+}
