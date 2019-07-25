22264840238da6caed470671bd7090aa72bada92
Hubert <leo.hubert3@gmail.com>
2019-07-25 15:22:19 +0200

    api change

--- fs-regenize-api/index.js
+++ fs-regenize-api/index.js
@@ -11,8 +11,8 @@ const logger = (req,res,next) => {
     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
 }
-app.use(logger);
 
+app.use(logger);
 app.use('/api/user', usersRouter);
 app.use('/api/auth', require('./src/api/auth-routes'));
  
