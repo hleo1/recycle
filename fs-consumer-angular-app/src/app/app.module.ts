7b1e94470486fa5456716995db3817fba84249b7
Braden <bbrown@oldmutual.com>
2019-07-29 22:30:54 +0200

    Added vanilla angular base app

---
+++ fs-consumer-angular-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material';
+import { LoginComponent } from './login/login.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
